d31402786d663561e9766c05050c860698290d03
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-06-16 20:22:53 -0300

    Final code

--- tp02.cpp
+++ tp02.cpp
@@ -4,48 +4,65 @@
 
 using namespace std;
 
+// Struct to represent an one-way road between two cities and their road's capacity
 struct Edge {
     int originNode;
     int destinationNode;
     int maxWeight;
 
+    // Operator overloading to compare two edges based on their weights
     bool operator<(const Edge& another) const
     {
         return maxWeight < another.maxWeight;
     }
 };
 
-void getCapacitiesFromNode(int originNode, vector<vector<Edge>> & adjacencyList, vector<vector<int>> & capacities) {
+int getCapacitiesFromNode(int originNode, int destinationNode, vector<vector<Edge>> & adjacencyList, vector<vector<int>> & capacities) {
+    // Initialize priority queue with all edges from originNode and update the capacities matrix
     vector<bool> visited(capacities.size(), false);
     priority_queue<Edge> pq;
+    int maxCapacity = 0;
 
     visited[originNode] = true;
     capacities[originNode][originNode] = 100000;
+
     for (int i = 0; i < adjacencyList[originNode].size(); i++) {
         Edge edge = adjacencyList[originNode][i];
         capacities[originNode][edge.destinationNode] = edge.maxWeight;
         pq.push(edge);
     }
 
+    // While there are edges in the priority queue and the destination node has not been visited, continue
     while (!pq.empty()) {
+        // Get the edge with the maximum weight
         Edge edge = pq.top();
         pq.pop();
+        
+        // If the node has already been visited, ignore it
+        if (edge.destinationNode == originNode || visited[edge.destinationNode]) {
+            continue;
+        }
 
+        // Update the capacities matrix at the destination node's index
         if (edge.maxWeight > capacities[originNode][edge.destinationNode]) {
             capacities[originNode][edge.destinationNode] = min(capacities[originNode][edge.originNode], edge.maxWeight);
         }        
+        
+        // Arrived at destination node
+        if (edge.destinationNode == destinationNode) {
+            maxCapacity = capacities[originNode][edge.destinationNode];
+            return maxCapacity;
+        }
 
+        // Add edges to priority queue
         visited[edge.destinationNode] = true;
         for (int i = 0; i < adjacencyList[edge.destinationNode].size(); i++) {
             Edge nextEdge = adjacencyList[edge.destinationNode][i];
-            if (nextEdge.destinationNode == originNode || visited[nextEdge.destinationNode]) {
-                continue;
-            }
             
             pq.push(nextEdge);
         }
     }  
-    return;
+    return maxCapacity;
 }
 
 int main() {
@@ -53,9 +70,11 @@ int main() {
     int numberOfRoads;
     int numberOfQueries;
 
+    // Get the number of cities, roads and queries
     cin >> numberOfCities >> numberOfRoads >> numberOfQueries;
     vector<vector<Edge>> adjacencyList(numberOfCities + 1);
 
+    // Get the roads and their capacities, creating the adjacency list 
     for (int i = 0; i < numberOfRoads; i++)
     {
         Edge aux;
@@ -63,6 +82,7 @@ int main() {
         adjacencyList[aux.originNode].push_back(aux);
     }
     
+    // Get the queries and print their results
     vector<bool> nodesAlreadyCalculated(numberOfCities + 1, false);
     vector<vector<int>> capacities(numberOfCities + 1, vector<int>(numberOfCities + 1, 0));
     for (int i = 0; i < numberOfQueries; i++) {
@@ -71,18 +91,9 @@ int main() {
         
         cin >> originNode >> destinationNode;
         
-        if (!nodesAlreadyCalculated[originNode]) {
+        int maxCapacity = getCapacitiesFromNode(originNode, destinationNode, adjacencyList, capacities);
 
-            getCapacitiesFromNode(originNode, adjacencyList, capacities);
-        }
-        
-        if (i != numberOfQueries - 1) {
-            cout << capacities[originNode][destinationNode] << endl;
-        }
-        else {
-            cout << capacities[originNode][destinationNode];
-        }
-        
+        cout << maxCapacity << endl;
     }
     
     return 0;
